9a29960463e1b6c267fc5cb790360ad3f1dfcde3
Mario Iglesias <marioip@gmail.com>
2014-02-03 12:34:40 +0100

    algunos cambios en la cabecera en el pie, favicon, screenshot

--- footer.php
+++ footer.php
@@ -9,15 +9,31 @@
 
 <footer class="full-width" role="contentinfo">
 	<div class="row">
-		<div class="large-12 columns">
-			<?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list')); ?>
-		</div>
+		<div class="small-12 large-12 columns text-center">
+        <a href="#/"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo-empty.png"></a>
+      	</div>
+      	<div class="small-12 large-12 columns text-center slogan slogan-foot">
+        	<h3>Mueve el mundo <strong class="mundoludic">MUNDO LUDIC</strong></h3>
+      	</div>
 	</div>
-	<div class="row love-reverie">
+<!-- 	<div class="row">
 		<div class="large-12 columns">
-			<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php _e('Made with Love in','reverie'); ?> <a href="http://themefortress.com/reverie/" rel="nofollow" title="Reverie Framework">Reverie</a>.</p>
+			<p>&copy; <?php echo date('Y'); ?> Mundo Ludic</p>
 		</div>
-	</div>
+	</div> -->
+	<div class="row">
+		<div class="small-12 large-12 columns">
+
+	          <div class="small-6 large-6 columns">
+	            <p>&copy; <?php echo date('Y'); ?> Mundo Ludic</p>
+	          </div>
+	        <div class="small-6 large-6 columns text-right social-foot">
+	          <a href="https://www.facebook.com/mundoludic.Page" target="_blank"><img class="facebook-grey" src="<?php echo get_template_directory_uri(); ?>/images/icons/social-icon-empty.png"></a>
+	          <a href="https://twitter.com/MundoLudic" target="_blank"><img class="twitter-grey" src="<?php echo get_template_directory_uri(); ?>/images/icons/social-icon-empty.png"></a>
+	        </div>
+
+	      </div>
+      </div>
 </footer>
 
 <?php wp_footer(); ?>
