727579055ebd96e2ea81901c1356e0f12bdc3479
Mario Iglesias <marioip@gmail.com>
2014-12-03 17:31:19 +0100

    update de single.php (comentarios despues de post relacionados)

--- single.php
+++ single.php
@@ -5,39 +5,22 @@
 	
 	<?php /* Start loop */ ?>
 	<?php while (have_posts()) : the_post(); ?>
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<header>
+		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">	
+                        <header>
 				<h1 class="entry-title"><?php the_title(); ?></h1>
 				<?php the_date(); ?>
 			</header>
 			<div class="entry-content">
 				<?php the_content(); ?>
+				<?php comments_template(); ?>
 			</div>
 			<footer>
+<a href="http://www.mundoludic.com/"><img src="http://www.mundoludic.com/blog/media/Visita-Mundo-Ludic-Reserva-de-artistas-de-animación-infantil.jpg" alt="Mundo Ludic, portal de animación infantil y actividades educativas" border="0" /></a>   
 				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'reverie'), 'after' => '</p></nav>' )); ?>
 				<p class="entry-tags"><?php the_tags(); ?></p>
 				<?php edit_post_link('Edit this Post'); ?>
 			</footer>
-		</article>
-		<!-- AddToAny BEGIN -->
-			<div class="a2a_kit a2a_kit_size_32 a2a_default_style">
-			<a class="a2a_dd" href="http://www.addtoany.com/share_save"></a>
-			<a class="a2a_button_facebook"></a>
-			<a class="a2a_button_twitter"></a>
-			<a class="a2a_button_google_plus"></a>
-			<a class="a2a_button_linkedin"></a>
-			<a class="a2a_button_email"></a>
-			</div>
-			<script type="text/javascript">
-			var a2a_config = a2a_config || {};
-			a2a_config.locale = "es";
-			a2a_config.color_main = "D7E5ED";
-			a2a_config.color_border = "AECADB";
-			a2a_config.color_link_text = "333333";
-			a2a_config.color_link_text_hover = "333333";
-			</script>
-			<script type="text/javascript" src="//static.addtoany.com/menu/page.js"></script>
-		<!-- AddToAny END -->
+</article>
 		<!-- <div class="entry-author panel">
 			<div class="row">
 				<div class="large-3 columns">
@@ -49,11 +32,10 @@
 				</div>
 			</div>
 		</div> -->
-		<br>
-		<?php comments_template(); ?>
+                &nbsp;
+       
 	<?php endwhile; // End the loop ?>
-
 	</div>
 	<?php get_sidebar(); ?>
-		
+
 <?php get_footer(); ?>
